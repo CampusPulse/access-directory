f4adf701dea218925af098ab768d9e2595ff06ff
Adrian Edwards <adrian@adriancedwards.com>
2025-03-31 23:48:07 -0400

    prod logging setup

--- app.py
+++ app.py
@@ -1582,3 +1582,7 @@ if __name__ == "__main__":
         app.run(host="0.0.0.0", port=8080)
     else:
         app.run()
+else: # prod
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
