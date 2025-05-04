425fd2c35abde95c8575bde342de5e0bc340f3f6
Airship46n <nathanielsleslie@gmail.com>
2025-05-04 14:11:03 -0400

    Updated Page Title
    Updated page title pathing in app.py to hopefully display page titles

--- app.py
+++ app.py
@@ -794,12 +794,12 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html" pageTitle="About")
 
 
 @app.route("/fmsreport")
 def fmsreport():
-    return render_template("fmsreport.html")
+    return render_template("fmsreport.html" pageTitle="Reporting to the RIT Service Center")
 
 
 @app.route("/map")
