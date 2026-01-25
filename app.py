ee55621dc102f6d1a8cf5e5e98f9bcd091953e58
Adrian Edwards <adrian@adriancedwards.com>
2026-01-25 16:47:33 -0500

    forgot to import the smart_add_status_report function

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ import json_log_formatter
 from pathlib import Path
 from dotenv import load_dotenv
 from helpers import floor_to_integer, RoomNumber, integer_to_floor, MapLocation, ServiceNowStatus, ServiceNowUpdateType, FMSSheetUpdateType, save_user_details, check_for_admin_role, get_logged_in_user_id, get_logged_in_user, get_logged_in_user_info, validate_work_order, clean_work_order
-from db_helpers import latest_status_for,highest_report_for 
+from db_helpers import latest_status_for,highest_report_for, smart_add_status_report
 from urllib.parse import quote_plus, urlencode
 from authlib.integrations.flask_client import OAuth
 
